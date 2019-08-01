429f377a9fedf99046bb6b8444e85f150e1ea4a1
Yoshihiro Fukuhara <gatheluck@Yoshihiros-MacBook-Pro.local>
2019-08-02 08:04:16 +0900

    add setup script for mac

---
+++ mac_setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+usage ()
+{
+	local script_name=$(basename "$0")
+	cat << END
+Usage: $script_name ENV
+Setup and install base applications.
+
+	ENV		current environment.
+
+Examples:
+	$script_name waseda
+END
+}
+
+# if number of option is 0, run usage().
+if [ "$#" -eq 0 ]; then
+	usage
+	exit １
+fi
+
+ENV="vanilla"
+
+#proxy
+if [ $1 = "waseda" ]; then
+	ENV="waseda"
+	PROXY="www-proxy.waseda.jp"
+	PORT="8080"
+fi
+
+if [ $ENV != "vanilla" ]; then
+	echo "proxy env: ${ENV}"
+	export http_proxy=http://${PROXY}:${PORT}
+	export https_proxy=$http_proxy
+	export ALL_PROXY=$http_proxy
+fi
+  
+
+#homebrew
+yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+
+brew_array=(\
+		git \
+		zsh \
+		zsh-completions \
+)
+
+cask_array=(\
+		iterm2 \
+		google-chrome \
+		slack \
+		visual-studio-code \
+		miniconda \ 
+)
+
+# install by homebrew
+for e in ${brew_array[@]}; do
+	brew install ${e}
+done
+
+# install by homebrew cask
+for e in ${cask_array[@]}; do
+	brew cask install ${e}
+done
