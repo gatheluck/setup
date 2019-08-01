985b54697a6c47c3cb0b0ccdde3dc102462fcd87
Yoshihiro Fukuhara <gatheluck@Yoshihiros-MacBook-Pro.local>
2019-08-02 08:06:24 +0900

    add script for zsh setup

---
+++ zsh_setup.sh
@@ -0,0 +1,49 @@
+#!/bin/zsh
+
+usage ()
+{
+	local script_name=$(basename "$0")
+	cat << END
+Usage: $script_name ENV
+Setup and install base applications.
+
+	ENV		current environment.
+
+Examples:
+	$script_name waseda
+END
+}
+
+# if number of option is 0, run usage().
+if [ "$#" -eq 0 ]; then
+	usage
+	exit １
+fi
+
+ENV="vanilla"
+
+#proxy
+if [ $1 = "waseda" ]; then
+	ENV="waseda"
+	PROXY="www-proxy.waseda.jp"
+	PORT="8080"
+fi
+
+if [ $ENV != "vanilla" ]; then
+	echo "proxy env: ${ENV}"
+	export http_proxy=http://${PROXY}:${PORT}
+	export https_proxy=$http_proxy
+	export ALL_PROXY=$http_proxy
+fi
+
+# use Prezto: https://github.com/sorin-ionescu/prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+# make config files
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# chenge login shell to zsh
+chsh -s /bin/zsh
